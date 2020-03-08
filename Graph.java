52f160156bc52e77af0122bc504d3c68c7b58222
togikirankumar <coolkiran373@gmai.com>
2020-03-09 02:00:04 +0530

    bfstopologicalsort

--- Graph.java
+++ Graph.java
@@ -119,6 +119,53 @@ class Graph{
 
     	return component;
     }
+
+    void topologicalSort(){
+
+    	HashMap<Integer,Integer>indgree=new HashMap<>();
+		
+    	for(Map.Entry node:adjList.entrySet())
+    	{
+    		indgree.put((int)node.getKey(),0);
+    	}
+
+		for(Map.Entry node:adjList.entrySet())
+		{
+			for(int neigh:adjList.get(node.getKey()))
+			{
+				int r=indgree.get(neigh)+1;
+				indgree.put(neigh,r);
+			}
+		
+		}    	
+
+		Queue<Integer>q=new LinkedList<>();
+
+		for(Map.Entry node:adjList.entrySet())
+		{
+			int nodex=(int)node.getKey();
+			if(indgree.get(nodex)==0)
+			{
+				q.add(nodex);
+			}
+		}
+		//for(Map.Entry mp:indgree)
+		while(!q.isEmpty())
+		{
+			Integer node=q.peek();
+			q.remove();
+			System.out.print(node+"->");
+			for(Integer neigh:adjList.get(node))
+			{
+				int r=indgree.get(neigh)-1;
+				indgree.put(neigh,r);
+				if(indgree.get(neigh)==0)
+				{
+					q.add(neigh);
+				}
+			}
+		}
+    }
 }
 
 class run{
@@ -126,27 +173,43 @@ class run{
 	public static void main(String[] args) {
 		Graph gg=new Graph();
 
-		gg.addEdge(1,2,true);
-		gg.addEdge(1,3,true);
-		gg.addEdge(2,4,true);
-		gg.addEdge(2,5,true);
-		gg.addEdge(15,20,true);
-		gg.addEdge(26,25,true);
+		// gg.addEdge(1,2,true);
+		// gg.addEdge(1,3,true);
+		// gg.addEdge(2,4,true);
+		// gg.addEdge(2,5,true);
+		// gg.addEdge(15,20,true);
+		// gg.addEdge(26,25,true);
 
 		//gg.addEdge(3,4,true);
+
+		gg.addEdge(1,2,false);
+		gg.addEdge(3,2,false);
+		gg.addEdge(2,4,false);
+		gg.addEdge(2,5,false);
+		gg.addEdge(4,6,false);
+		gg.addEdge(5,6,false);
+		gg.addEdge(6,7,false);
+		gg.addEdge(5,7,false);
+		
 
 		gg.display();
 
 		System.out.println("*********DFS*********");
-		gg.dfs(15);
+		gg.dfs(1);
 
 		System.out.println();
 		System.out.println("*********BFS**********");
-		gg.bfs(15);
+		gg.bfs(1);
 
 		System.out.println();
 		System.out.println("*********connectedComponents**********");
 		int x=gg.connectedComponents();
 		System.out.println(x);
+
+		System.out.println();
+		System.out.println("*********bfs topologicalSort**********");
+		gg.topologicalSort();
+		
+
 	}
 }
